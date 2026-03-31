7ba0c3496a4d5f3b20d7f716f91f47f98162a08a
Fabricio <50778242+fabriciosouza0@users.noreply.github.com>
2026-03-31 07:47:08 -0300

    feat: Adiciona config do spring batch e job para importação de clientes via csv.

--- spring-batch-csv-import/src/main/java/com/fabricio/spring/batch/repository/CustomerJdbcRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fabricio.spring.batch.repository;
-
-import com.fabricio.spring.batch.entity.CustomerEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerJdbcRepository extends CrudRepository<CustomerEntity, Long> {
-}
